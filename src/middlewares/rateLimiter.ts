33d24a6606e2299fc28b9c0a50fd5e2a8466f53e
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-06-02 11:58:11 +0200

    fixed

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,21 +1,21 @@
 import {NextFunction, Request, Response} from "express";
 import rateLimit from "express-rate-limit";
 
-const endpointRateLimits = {
-  '/users/verification/send': {
+const endpointRateLimiters  = {
+  '/users/verification/send': rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 3, // limit each IP to 3 requests per windowMs
     message: 'Too many email verification requests created, please try again after 10 minutes.'
-  }
+  })
 };
 
 const rateLimiter = (req: Request, res: Response, next: NextFunction) => {
   try {
     const { url: endpoint } = req;
 
-    if (!(endpoint in endpointRateLimits)) return next();
+    if (!(endpoint in endpointRateLimiters)) return next();
 
-    return rateLimit(endpointRateLimits[endpoint as keyof typeof endpointRateLimits])(req, res, next);
+    return endpointRateLimiters[endpoint as keyof typeof endpointRateLimiters](req, res, next);
   } catch (err) {
     console.error(`Error limiting endpoint calls. Error: ${err}`);
     next(err);
