41b5b0b383a1f5fd97b7bd766ec0709ffc8d534b
G-C0der <31240102+G-C0der@users.noreply.github.com>
2023-08-29 07:55:52 +0200

    saving working on save endpoint

--- src/controllers/CalculatorController.ts
+++ src/controllers/CalculatorController.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import {serverError} from "../constants";
+import {CalculatorParams} from "../models";
 
 const save = async (req: Request, res: Response, next: NextFunction) => {
   try {
-
+    await CalculatorParams.create({ ...req.body, userId: req.user!.id });
   } catch (err) {
     console.error(`${serverError} Error: ${err}`);
     res.status(500).send(serverError);
