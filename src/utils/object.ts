51f4009b5623270bf71f2d3168b3a51c334801db
G-C0der <31240102+G-C0der@users.noreply.github.com>
2024-03-03 10:55:09 +0100

    hotfix prod params kettle logging

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -9,14 +9,32 @@ const getChangedProperties = <T extends object>(
   oldData: T,
   newData: T
 ): { oldData: Partial<T>, newData: Partial<T> } => Object.keys(oldData).reduce((acc, key) => {
-    const keyOfT = key as keyof T;
-    if (keyOfT in newData && !isEqual(oldData[keyOfT], newData[keyOfT])) {
-      acc['oldData'][keyOfT] = oldData[keyOfT];
-      acc['newData'][keyOfT] = newData[keyOfT];
-    }
-    return acc;
-  }, { oldData: {}, newData: {} } as { oldData: Partial<T>, newData: Partial<T> }
-);
+  const keyOfT = key as keyof T;
+  let oldProp = oldData[keyOfT];
+  let newProp = newData[keyOfT];
+
+  if (typeof oldProp === 'string' && isJsonString(oldProp)) oldProp = JSON.parse(oldProp);
+  if (typeof newProp === 'string' && isJsonString(newProp)) newProp = JSON.parse(newProp);
+
+  if (keyOfT in newData && !isEqual(oldProp, newProp)) {
+    acc['oldData'][keyOfT] = oldProp;
+    acc['newData'][keyOfT] = newProp;
+  }
+  return acc;
+}, { oldData: {}, newData: {} } as { oldData: Partial<T>, newData: Partial<T> });
+
+/**
+ * Helper function to check if a string is a valid JSON string
+ * @param str
+ */
+const isJsonString = (str: string): boolean => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
 export {
   getChangedProperties
